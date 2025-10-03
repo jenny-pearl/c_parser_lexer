cd407bfb48cab596a1d9efafc2662242597e6d66
Jenny <aalper0138@gmail.com>
2025-10-04 00:51:02 +0300

    Add files via upload

--- test.c
+++ test.c
@@ -1,10 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <assert.h>
 
 int main (void)
 { 
 	// This is a comment
+	/*
+	 * This is an
+	 * extended comment
+	 */
 	printf("Hello, \"World\n");
 	return (0); 
 }
